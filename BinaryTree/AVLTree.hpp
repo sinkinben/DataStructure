2d7b10c5e7b1e3733fb9b9efde23559f99d356d1
sinkinben <sinkinben@outlook.com>
2020-09-25 17:01:52 +0800

    feat: complete avl remove function

--- BinaryTree/AVLTree.hpp
+++ BinaryTree/AVLTree.hpp
@@ -74,7 +74,7 @@ public:
     bool insert(int val)
     {
         bool taller;
-        insert(this->root, val, taller, nullptr);
+        return insert(this->root, val, taller, nullptr);
         // std::cout << "111";
     }
     bool insert(TreeNode<T> *&subtree, T val, bool &taller, TreeNode<T> *parent)
@@ -162,12 +162,153 @@ public:
 
     bool remove(T val)
     {
-        return remove(this->root, val);
+        bool shorter = false;
+        return remove(this->root, val, shorter);
     }
 
-    bool remove(TreeNode<T> *&subtree, T val)
+    bool remove(TreeNode<T> *&subtree, T val, bool &shorter)
     {
+        // val is not in AVL
+        if (subtree == nullptr)
+        {
+            return false;
+        }
+        else if (subtree->val == val)
+        {
+            auto p = subtree;
+            bool hasLeft = p->left != nullptr;
+            bool hasRight = p->right != nullptr;
+            if (!hasLeft && hasRight)
+            {
+                subtree->right->parent = subtree->parent;
+                subtree = subtree->right;
+                delete p;
+                shorter = true;
+                return true;
+            }
+            if (hasLeft && !hasRight)
+            {
+                subtree->left->parent = subtree->parent;
+                subtree = subtree->left;
+                delete p;
+                shorter = true;
+                return true;
+            }
+            if (!hasLeft && !hasRight)
+            {
+                if (p == this->root)
+                {
+                    delete this->root;
+                    this->root = nullptr;
+                    return true;
+                }
+                if (p == p->parent->left)
+                    p->parent->left = nullptr;
+                else
+                    p->parent->right = nullptr;
+                delete p;
+                shorter = true;
+                return true;
+            }
+            if (hasLeft && hasRight)
+            {
+                // 把前驱的值搬上来，然后递归删除这个前驱
+                auto lmax = maxval(p->left);
+                subtree->val = lmax->val;
+                if (remove(subtree->left, lmax->val, shorter))
+                {
+                    if (shorter)
+                    {
+                        switch (subtree->factor)
+                        {
+                        case AVLFactor::LH:
+                            subtree->factor = AVLFactor::EH;
+                            shorter = true;
+                            break;
+                        case AVLFactor::EH:
+                            subtree->factor = AVLFactor::RH;
+                            shorter = false;
+                            break;
+                        case AVLFactor::RH:
+                            shorter = (subtree->right->factor == AVLFactor::EH) ? false : true;
+                            rightBalance(subtree);
+                            break;
+                        default:
+                            assert(0);
+                            break;
+                        }
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+        else if (val < subtree->val)
+        {
+            if (!remove(subtree->left, val, shorter))
+                return false;
+            if (shorter)
+            {
+                switch (subtree->factor)
+                {
+                case AVLFactor::LH:
+                    subtree->factor = AVLFactor::EH;
+                    shorter = true;
+                    break;
+                case AVLFactor::EH:
+                    subtree->factor = AVLFactor::RH;
+                    shorter = false;
+                    break;
+                case AVLFactor::RH:
+                    shorter = (subtree->right->factor == AVLFactor::EH) ? false : true;
+                    rightBalance(subtree);
+                    break;
+                default:
+                    assert(0);
+                    break;
+                }
+            }
+            return true;
+        }
+        else if (val > subtree->val)
+        {
+            if (!remove(subtree->right, val, shorter))
+                return false;
+            if (shorter)
+            {
+                switch (subtree->factor)
+                {
+                case AVLFactor::LH:
+                    shorter = (subtree->left->factor == AVLFactor::EH) ? (false) : (true);
+                    leftBalance(subtree);
+                    break;
+                case AVLFactor::EH:
+                    subtree->factor = AVLFactor::LH;
+                    shorter = false;
+                    break;
+                case AVLFactor::RH:
+                    subtree->factor = AVLFactor::EH;
+                    shorter = true;
+                    break;
+                default:
+                    assert(0);
+                }
+            }
+            return true;
+        }
+        return false;
     }
+
+    TreeNode<T> *maxval(TreeNode<T> *subtree)
+    {
+        if (subtree == nullptr)
+            return nullptr;
+        auto p = subtree;
+        while (p->right != nullptr)
+            p = p->right;
+        return p;
+    }
+    TreeNode<T> *maxval() { return maxval(this->root); }
 
     void leftBalance(TreeNode<T> *p)
     {
@@ -207,8 +348,12 @@ public:
             leftRotate(p->left);
             rightRotate(p);
             break;
+        // only used in remove operation
         case AVLFactor::EH:
-            assert(0);
+            p->factor = AVLFactor::LH;
+            p->left->factor = AVLFactor::RH;
+            rightRotate(p);
+            break;
         default:
             break;
         }
@@ -244,6 +389,12 @@ public:
             }
             rl->factor = AVLFactor::EH;
             rightRotate(p->right);
+            leftRotate(p);
+            break;
+        // only used in remove operation
+        case AVLFactor::EH:
+            p->factor = AVLFactor::RH;
+            p->right->factor = AVLFactor::LH;
             leftRotate(p);
             break;
         default:
--- BinaryTree/AVLTreeMain.cpp
+++ BinaryTree/AVLTreeMain.cpp
@@ -1,24 +1,53 @@
 #include "AVLTree.hpp"
-#include <iostream>
-#include <string>
+#include <sstream>
 using namespace std;
+AVLTree<int> tree;
+
+void dispatch(string &cmd)
+{
+    stringstream ss(cmd);
+    string name;
+    ss >> name;
+    if (name == "i")
+    {
+        int val;
+        while (ss >> val)
+            tree.insert(val);
+    }
+    else if (name == "r")
+    {
+        int val;
+        ss >> val;
+        tree.remove(val);
+    }
+    else if (name == "show")
+    {
+        tree.draw();
+    }
+    else
+    {
+        throw "Unknown command: " + name;
+    }
+}
 
 int main()
 {
-    string s;
-    AVLTree<int> avl;
-    while (getline(cin, s))
+    string cmd;
+    const string prompt = "> ";
+    cout << prompt;
+    while (true)
     {
-        if (s == "q")
+        getline(cin, cmd);
+        if (cmd == "exit" || cmd == "q")
             break;
-        stringstream ss(s);
-        int val;
-        while (ss >> val)
+        try
         {
-            // cout << val << endl;
-            avl.insert(val);
+            dispatch(cmd);
         }
-        // cout << "222";
-        avl.draw();
+        catch (const string msg)
+        {
+            std::cerr << msg << '\n';
+        }
+        cout << prompt;
     }
 }
